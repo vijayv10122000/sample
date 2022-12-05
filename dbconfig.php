f2ea58833b90047f8507d6efa06cc7f44ae35191
Vijay V <vijay@lyceumapp.com>
2022-12-05 16:52:37 +0530

    updated config

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect("localhost","root","","mycrazy");
-//  $conn = mysqli_connect("localhost","mycrazyx_lyceumapp","vijay@2000","mycrazyx_lyceumapp");
+// $conn = mysqli_connect("localhost","root","","mycrazy");
+ $conn = mysqli_connect("localhost","mycrazyx_lyceumapp","vijay@2000","mycrazyx_lyceumapp");
  session_start();
 // Check connection
 // if (!$conn) {
